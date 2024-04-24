8ba79b33ef6b5034480404c889c95b0fa7aa3e93
MartiSandoval <163368695+MartiSandoval@users.noreply.github.com>
2024-04-24 02:34:15 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -110,6 +110,7 @@ Pair * firstMap(HashMap * map) {
   long i = 0;
   while (i < map->capacity) {
     if (map->buckets[i] != NULL && map->buckets[i]->key != NULL) {
+      map->current = i;
       return map->buckets[i];
     }
     i = (i + 1) % map->capacity;
