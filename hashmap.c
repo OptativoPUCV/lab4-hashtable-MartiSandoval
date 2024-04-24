239582fddcad8c14168a95d82c433e9fda4017f0
MartiSandoval <163368695+MartiSandoval@users.noreply.github.com>
2024-04-24 00:33:12 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -40,8 +40,23 @@ int is_equal(void* key1, void* key2){
 
 
 void insertMap(HashMap * map, char * key, void * value) {
+  Pair *par = createPair(key, value);
+  long pos = hash(key, map->capacity);
 
-
+  if (map->buckets[pos] == NULL) {
+    map->buckets[pos]->key = key;
+    map->buckets[pos]->value = value;
+    map->size++;
+  }
+  else {
+    for (long i = 0; i < map->capacity; i++) {
+      if (is_equal(map->buckets[i]->key, key)) {
+        map->buckets[i]->key = key;
+        map->buckets[i]->value = value;
+      }
+    }
+  }
+  
 }
 
 void enlarge(HashMap * map) {
