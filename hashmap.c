566456ddd3cd4bcd57e6dba1db8a327632b6d47c
MartiSandoval <163368695+MartiSandoval@users.noreply.github.com>
2024-04-24 01:47:13 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -107,9 +107,13 @@ Pair * searchMap(HashMap * map,  char * key) {
 Pair * firstMap(HashMap * map) {
   long i = 0;
   while (map->buckets[i] == NULL) {
+    if (map->size == 0) {
+      return NULL;
+    }
     i = (i + 1) % map->capacity;
-    map->current = i;
+    
   }
+  
   return map->buckets[i];
 }
 
